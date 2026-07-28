ad84f334eed7d0409be3563da5a740be472f97ef
P0WER <power@example.com>
2026-07-29 02:48:10 +0700

    feat: add cookies.txt for YouTube authentication on Cloud Server

--- core/shared.py
+++ core/shared.py
@@ -61,10 +61,12 @@ YDL_OPTIONS = {
     'nocheckcertificate': True,
     'extractor_args': {
         'youtube': [
-            'player_client=ios,android'
+            'player_client=ios,android,web'
         ]
     }
 }
+if os.path.exists("cookies.txt"):
+    YDL_OPTIONS['cookiefile'] = "cookies.txt"
 import shutil
 
 def get_ffmpeg_executable():
